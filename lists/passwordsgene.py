28dda91bce4afcfc614609e61deabef48c34b970
oboyle3 <oboyle3patrick@gmail.com>
2026-09-13 17:05:45 -0400

    pw gene

--- lists/passwordsgene.py
+++ lists/passwordsgene.py
@@ -3,17 +3,31 @@
 #  uppercase letters, numbers, and symbols. The passwords should 
 #  be random, generating a new password every time the user asks for a new password
 import random , string
-#thinking genrate some random numbers special characters and numbers
-#generate random 3 letter num
-rand_num1 = random.randint(1,500)
-rand_num2 = random.randint(1,500)
-rand_num3 = random.randint(1,500)
-#genrate random letters
-rand_letter1 = random.choice(string.ascii_lowercase)
-print(f"rand_letter = {rand_letter1}")
-rand_letter2 = random.choice(string.ascii_lowercase)
-print(f"rand_letter = {rand_letter2}")
-rand_letter3 = random.choice(string.ascii_lowercase)
-print(f"rand_letter = {rand_letter3}")
-#generate randome symbol
-# endoutput = str(rand_num1 + rand_letter1)
+
+
+def give_pw():
+    rand_num1 = random.randint(1,500)
+    RANDOM_NUMBER1 = str(rand_num1)
+    rand_num2 = random.randint(1,500)
+    RANDOM_NUMBER2 = str(rand_num2)
+    rand_num3 = random.randint(1,500)
+    RANDOM_NUMBER3 = str(rand_num3)
+    #genrate random letters
+    rand_letter1upercase = random.choice(string.ascii_uppercase)
+    rand_letter1 = random.choice(string.ascii_lowercase)
+    print(f"rand_letter = {rand_letter1}")
+    rand_letter2 = random.choice(string.ascii_lowercase)
+    print(f"rand_letter = {rand_letter2}")
+    rand_letter3 = random.choice(string.ascii_lowercase)
+    print(f"rand_letter = {rand_letter3}")
+    print(f"RANDOM_NUMBER1 = {RANDOM_NUMBER1}")
+    print(f"RANDOM_NUMBER2 = {RANDOM_NUMBER2}")
+    print(f"RANDOM_NUMBER3 = {RANDOM_NUMBER3}")
+    #generate randome symbol
+    final_pw = rand_letter1upercase +RANDOM_NUMBER2 + rand_letter1+ RANDOM_NUMBER3 + rand_letter2 + rand_letter3 + RANDOM_NUMBER1 
+    print(f" final pw the user can use: {final_pw}")
+
+give_pw()
+
+
+
